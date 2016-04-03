ea5d421f48c50d5dc57b9268a6d81d2a241a181d
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2016-04-04 00:08:39 +0700

    -

--- server.js
+++ server.js
@@ -1,24 +1,50 @@
 'use strict';
 
 const Hapi = require('hapi');
-
 const server = new Hapi.Server();
+
 server.connection({ port: 3000 });
 
+var r = require('rethinkdb')
+var cn = null;
+r.connect( {host: 'archera', port: 28015, db: 'bookstera'}, function(err, conn) {
+  if (err) throw err;
+  cn = conn;
+})
+
 server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!');
-    }
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    r.db('rethinkdb').table('server_config').run(cn, function(err, result) {
+        if (err) {
+          reply('error');
+        } else {
+          reply(result.toArray());
+        }
+    })
+  }
 });
 
-server.start((err) => {
+server.route({
+  method: 'GET',
+  path: '/resources.json',
+  handler: function (request, reply) {
+    r.db('bookstera').table('resources').run(cn, function(err, result) {
+        if (err) {
+          reply('error');
+        } else {
+          reply(result.toArray());
+        }
+    })
+  }
+});
 
+
+
+server.start((err) => {
   if (err) {
     throw err;
   }
-
   console.log('Server running at:', server.info.uri);
-
 });
